1abb377af71465e785e539fc4341c4c62b24ec2a
efrén <147435602+n2rfe@users.noreply.github.com>
2025-02-03 05:15:42 +0000

    final commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,33 @@
+public class BankAccount {
+
+  public static void main(String[] args) {
+
+  // Bob's initial balance
+  double balance = 2175.37;
+
+  // 1
+  balance -= 302.50;
+
+  // 2
+  balance += 50.03;
+
+  // 3
+  balance /= 2;
+
+  // 4
+  balance += 20.00;
+
+  // 5
+  balance -= 1;
+
+  // 6
+  balance *= 2;
+
+  // 7
+  balance += 1;
+
+  // Bob's final balance
+  System.out.println("Bob's final balance is: $" + balance);
+  }
+}
 
